38af51d3eff8a5d855d7e3551a906faa3e2fc9c7
eyerusalemasrat <45384135+eyerusalemasrat@users.noreply.github.com>
2020-02-20 00:30:35 +0300

    final

--- src/residential/views.py
+++ src/residential/views.py
@@ -21,19 +21,8 @@ def Residential(request):
     user.type = request.POST['type']
     user.save()
 
-    print(user.type)
+  
 
-    if user.type == 'Business_Mobile':
-        
-        return render(request, 'Business_Mobile.html')
-    elif user.type == 'Broadband':
-        return render(request, 'Broadband.html')
-    elif user.type == 'Bill_Collection':
-        return render(request, 'Bill_Collection.html')
-    elif user.type == 'Hybrid':
-        return render(request, 'Hybrid.html')
-    elif user.type == "FixedLine":
-        return render(request, 'FixedLine.html')
-    return render(request, "Personal.html")
+    return render(request,'TokenNumber.html',context)
 
 
--- static/js/index.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-
-        if(document.getElementById('keyaccount').checked) {
-            document.getElementById('generateToken').href = "location.href='../keyAccount'";
-        }
-        else{
-        	document.getElementById('generateToken').href = "location.href='../sohom'";
-        }
-    }
